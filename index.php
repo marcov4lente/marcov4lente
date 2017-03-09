a2228677e1dad22f5421a776375cc9d5c3618994
Marco Valente <mvalente@britishhorseracing.com>
2017-03-09 15:57:39 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+        <div class="row">
+            <div class="col s1">
+                <div class="row">
+                    <div class="col s1 m6">Logo</div>
+                    <div class="col s1 m6">
+                        Linked In
+                        Instagram
+                        Quote
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
