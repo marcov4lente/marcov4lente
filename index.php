db99aeaea80bb5d926c29994d94b78fda5bdddf9
Marco Valente <Marco Valente>
2017-03-13 22:10:58 +0000

    Development update

--- index.php
+++ index.php
@@ -1,26 +1,73 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>MarcoV4lente - Web Application Developer</title>
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
 
+        <nav class="white blue-grey-text text-darken-2">
+            <div class="container">
+                <div class="nav-wrapper">
+                <a href="#" class="logo"><img src="images/marcov4lente-logo.png" alt="MarcoV4lente"></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="https://www.linkedin.com/in/marcov4lente/" class="red-text text-darken-4"><i class="fa fa-linkedin-square" aria-hidden="true"></i> Linked In</a></li>
+                <li><a href="https://instagram.com/marcov4lente" class="red-text text-darken-4"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="row hero">
+
+            <div class="container">
+                <div class="col hide-on-small-and-down m2">
+                    &nbsp;
+                </div>
+                <div class="col s12 m3 avatar">
+                    <img src="images/Marco-Valente-Avatar.jpg" alt=Marco V4lente Avatar">
+                </div>
+                <div class="col s12 m5 quote">
+                    Hi, I'm Marco! I'm a hard working and ambitious Web Application developer with keen adherence coding standards.
+                </div>
+                <div class="col hide-on-small-and-down m2">
+                    &nbsp;
+                </div>
+
+            </div>
+        </div>
         <div class="row">
-            <div class="col s1">
-                <div class="row">
-                    <div class="col s1 m6">Logo</div>
-                    <div class="col s1 m6">
-                        Linked In
-                        Instagram
-                        Quote
-                    </div>
+            <div class="container">
+
+                <div class="col s12 m4">
+                    <p>
+                        With eight years of practical commercial experience, I am both adaptable and resourceful while maintaining an ordered, organised approach to my ongoing projects.
+                    </p>
+                </div>
+                <div class="col s12 m4">
+                    <p>
+                        My self motivation and a fascination for web technologies consistently fuels my endeavours for continued professional growth and advancement. As a result I am always eager to expand, learn and adapt to new techniques in a continuously evolving field.
+                    </p>
+                </div>
+                <div class="col s12 m4">
+                    <p>
+                        I find pleasure in conceptualising and building functional PHP applications that serve purpose. I am a firm advocate for standards and best practices, which I believe are the cornerstones of beautifully constructed applications.
+                    </p>
                 </div>
             </div>
         </div>
-
+        <footer class="page-footer white">
+            <div class="copyright blue-grey-text text-darken-2">
+                <p>
+                    © <?php print date('Y') ?> Copyright MarcoV4lente, property on Marco Valente, all rights reserved.
+                </p>
+            </div>
+        </footer>
         <!--Import jQuery before materialize.js-->
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
